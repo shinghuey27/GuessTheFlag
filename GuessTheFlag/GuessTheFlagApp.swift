cca7f2ec5b14f2ce6dbc6e2c8125c8d5f7ff5928
shinghuey27 <shinghuey27@hotmail.com>
2024-06-03 16:02:47 +0800

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Shing Huey on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
